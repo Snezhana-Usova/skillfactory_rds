37dc8a60edaa724528e6ae022e4a15691395fc95
Snezhana-Usova <snowsnezha@gmail.com>
2020-04-21 16:28:37 +0300

    Code adjusted and display added

--- module_0/main.py
+++ module_0/main.py
@@ -1,1 +1,29 @@
-print("Hello world")
+import numpy as np
+def game_core(number):
+    count = 1
+    predict = np.random.randint(1,101)
+    while number != predict:
+        count+=1
+        if number > predict: 
+            while number > predict:
+                count+=1
+                predict += 10
+                while number < predict:
+                    count+=1
+                    predict -= 1
+        elif number < predict: 
+            while number < predict:
+                count+=1
+                predict -= 10
+                while number > predict:
+                    count+=1
+                    predict += 1
+    return(count) 
+def score_game(game_core):
+    count_ls = []
+    np.random.seed(1)  
+    random_array = np.random.randint(1,101, size=(1000))
+    for number in random_array:
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
